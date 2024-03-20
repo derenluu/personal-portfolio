afcbf59c15b30f27f8b7122255d0978ee50985da
Deren <danhluu.business@gmail.com>
2024-03-20 14:27:09 +0700

    Load Again

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,14 @@
 export interface ArticlesType {
   title: string;
   status: string;
-  sections: any;
-  // sections: JSX.Element; // JSX.Element là kiểu dữ liệu cho các thành phần React
+  // sections: any;
+  sections: JSX.Element; // JSX.Element là kiểu dữ liệu cho các thành phần React
 }
 export interface ArticlesProjectType {
   title: string;
   status: string;
-  sections: any;
-  // sections: JSX.Element; // JSX.Element là kiểu dữ liệu cho các thành phần React
+  // sections: any;
+  sections: JSX.Element; // JSX.Element là kiểu dữ liệu cho các thành phần React
 }
 export interface NavBarType {
   onTabChange: (tabName: string) => void;
@@ -20,6 +20,12 @@ export interface FilterListType {
 }
 
 export interface FilterItemType {
+  title: string;
+  status: string;
+  onClick: () => void; // onClick là một hàm không có đối số và không trả về giá trị
+}
+
+export interface NavItemType {
   title: string;
   status: string;
   onClick: () => void; // onClick là một hàm không có đối số và không trả về giá trị
