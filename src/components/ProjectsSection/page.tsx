5814a6f7617da021b6f9def7ffa814b708c1a913
Deren <danhluu.business@gmail.com>
2024-03-20 14:11:18 +0700

    Fix code Projects Section

--- src/components/ProjectsSection/page.tsx
+++ src/components/ProjectsSection/page.tsx
@@ -1,50 +1,57 @@
 'use client';
 
-import {useState} from 'react';
-import {Data} from '@/data';
-import ArticlesProject from './ArticlesProject';
-import PrivateProject from './TypeProject/PrivateProject';
-import PublicProject from './TypeProject/PublicProject';
-import FilterListProject from './FilterListProject';
+// import {useState} from 'react';
+// import {Data} from '@/data';
+// import ArticlesProject from './ArticlesProject';
+// import PrivateProject from './TypeProject/PrivateProject';
+// import PublicProject from './TypeProject/PublicProject';
+// import FilterListProject from './FilterListProject';
 
 export function ProjectsSection() {
-  const [activeTab, setActiveTab] = useState('Public');
+  //   const [activeTab, setActiveTab] = useState('Public');
 
-  // declare variable types
-  const handleTabChange = (tabName: string) => {
-    setActiveTab(tabName);
-  };
+  //   // declare variable types
+  //   const handleTabChange = (tabName: string) => {
+  //     setActiveTab(tabName);
+  //   };
 
   return (
-    <>
-      <section className="project">
-        <FilterListProject
+      <>
+        <section className="project">~
+          {/* <p>{Data.portfolio.Achievements.Activities}</p> */}
+          <p>Projects</p>
+        </section>
+      </>
+    // <>
+      {/* <section className="project"> */}
+      {/* Projects */}
+      {/* <FilterListProject
           onTabChange={handleTabChange}
           activeTab={activeTab}
         />
         {activeTab === 'Public' && <PublicTab />}
-        {activeTab === 'Private' && <PrivateTab />}
-      </section>
-    </>
+        {activeTab === 'Private' && <PrivateTab />} */}
+      {/* </section> */}
+    // </>
   );
 }
 
-function PublicTab() {
-  return (
-    <ArticlesProject
-      title="Public"
-      status="active"
-      sections={<PublicProject />}
-    />
-  );
-}
+// function PublicTab() {
+//   return (
+//     <ArticlesProject
+//       title="Public"
+//       status="active"
+//       sections={<PublicProject />}
+//     />
+//   );
+// }
 
-function PrivateTab() {
-  return (
-    <ArticlesProject
-      title="Private"
-      status="active"
-      sections={<PrivateProject />}
-    />
-  );
-}
+// function PrivateTab() {
+//   return (
+//     <ArticlesProject
+//       title="Private"
+//       status="active"
+//       sections={<PrivateProject />}
+//     />
+//   );
+// }
